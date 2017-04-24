21c760e38ba989c75a2ff58edaae52c52dfef7a6
Elton Islamaj <elton.islamaj@gmail.com>
2017-04-23 21:32:14 -0400

    use of cut & sed in regular expression
    how to use cut & sed when doing regular expression in kali linux

--- cut_sed_regex.sh
+++ cut_sed_regex.sh
@@ -8,20 +8,19 @@ name_address=$(faker profile name,address -r 100)
 # save the result as txt & csv file 
 for adr in "$name_address"
 do
-echo "$adr" > name_address.txt
-echo "$adr" > name_address.csv
+  echo "$adr" > name_address.txt
+  echo "$adr" > name_address.csv
 done
 
 # use regular expression below on the 
 # name-address.txt file created above.
 while read line 
 do
-# Extract only the names, address from the txt file above &
-# get rid of curly brackets and the first comma 
-state=$(echo $line | cut -d " " -f 2,3,5,6,7,8,9,10,11,12 | 
+  # Extract only the names, address from the txt file above &
+  # get rid of curly brackets and the first comma 
+  initials=$(echo $line | cut -d " " -f 2,3,5,6,7,8,9,10,11,12 | 
               sed "s/}//" | sed "s/,//")
-echo $state
-echo "$state" >> states2.txt
-echo "$state" >> states2.csv
+  echo $initials
+  echo "$initials" >> states.txt
 done < name_address.txt
 
