6de4ce08f607fe24cdbbba883f64c71f7052ba35
Elton Islamaj <elton.islamaj@gmail.com>
2017-04-28 01:17:36 -0400

    regex in kali linux
    regular expression in kali linux

--- regular_expression.sh
+++ regular_expression.sh
@@ -7,12 +7,10 @@ echo $(paste <(faker name -r 100) <(faker address -r 100)) > name-address.txt
 # Extract only the state initials from the txt file above
 while read line 
 do
-
-state=$(echo $line | egrep -o ',?/*[A-Z]{2}+')
-# print variable state to see result
-echo $state
-# save variable state as a txt or csv file
-echo "$state" > states.txt
-echo "$state" > states.csv
-
+  state=$(echo $line | egrep -o ',?/*[A-Z]{2}+')
+  # print variable state to see result
+  echo $state
+  # save variable state as a txt or csv file
+  echo "$state" > states.txt
+  echo "$state" > states.csv
 done < name-address.txt
